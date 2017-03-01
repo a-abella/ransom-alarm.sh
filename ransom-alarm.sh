5903bc7f050ddbb22bcafe6bd8f23f78e86b2b72
Antonio Abella <aabel011@fiu.edu>
2017-02-28 19:40:16 -0500

    .

--- ransom-alarm.sh
+++ ransom-alarm.sh
@@ -2,7 +2,7 @@
 count=0
 
 declare -A shares
-shares=( ["smb-dfs00"]="IT" ["smb-dfs01"]="ClientServices" ["smb-dfs02"]="Operations" ["smb-dfs03"]="QualityAssurance" ["smb-dfs04"]="Training" )
+shares=( ["share00"]="IT" ["share01"]="ClientServices" ["share02"]="Operations" ["share03"]="QualityAssurance" ["share04"]="Training" )
 thisShare="${shares[$HOSTNAME]}"
 
 while :
@@ -10,12 +10,12 @@ do
 	{ inotifywait -e modify,delete,move,delete_self,move_self /share/*/_Locker_Defense/* && let count="$count + 1"; } || exit 1
 	if [ "$count" -gt "15" ]; then
 		smbaccess=$(smbstatus)
-		echo "From: "$thisShare"Share@"$HOSTNAME".inktel.com" >> /root/tempmessage.txt
-		echo "To: itsupport@inktel.com" >> /root/tempmessage.txt
+		echo "From: "$thisShare"Share@"$HOSTNAME".company.tld" >> /root/tempmessage.txt
+		echo "To: itsupport@company.tld" >> /root/tempmessage.txt
 		echo "Subject: Suspicious activity on $thisShare share: $count inodes modified" >> /root/tempmessage.txt
 		echo -e "Suspicious activity on $thisShare share: $count inodes modified.\n" >> /root/tempmessage.txt
 		echo "$smbaccess" >> /root/tempmessage.txt
-		sendmail itsupport@inktel.com < /root/tempmessage.txt
+		sendmail itsupport@company.tld < /root/tempmessage.txt
 		rm -f /root/tempmessage.txt
 		count=-250
 	fi
